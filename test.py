ff4bb4475c90f37dba30e3b0f225b4442bade68a
Karl Rydberg <kalle.rydberg02@gmail.com>
2025-10-25 06:43:18 +0200

    Added rectangle class to testfile

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+import matplotlib as plt
+from matplotlib.patches import Rectangle, Circle
+
 class Circle:
-    def __init__(self, x, y, radius):
+    def __init__(self, radius, x=0, y=0):
         self.x = x
         self.y = y
         self.radius = radius
@@ -33,6 +36,8 @@ class Circle:
     def radius(self, new_radius):
         if not isinstance(new_radius, (int, float)):
             raise TypeError("Radius must be an integer or float")
+        elif new_radius <= 0:
+            raise ValueError("Radius must be greater than 0")
         self._radius = new_radius
     @property
     def area(self):
@@ -55,7 +60,7 @@ class Circle:
     def __str__(self):
         return f"Circle object with coordinates ({self.x}, {self.y})"
     def translate(self, new_x, new_y):
-        if not isinstance(new_x, (int, float)):
+        if not isinstance(new_x, (int, float)) or not isinstance(new_y, (int, float)):
             raise TypeError(f"Coordinates must be int or float")
         else:
             if 10 < new_x < -10 or 10 < new_y < -10:
@@ -68,9 +73,89 @@ class Circle:
         else:
             return False
 
-circle1 = Circle(0, 0, 1)
-circle2 = Circle(1, 3, 4)
 
-circle1.translate(1, "2")
 
-print(circle1)
+
+class Rectangle:
+    def __init__(self, height, width, x=0, y=0):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.area_ = self.height * self.width
+    
+    @property
+    def x(self):
+        return self._x
+    @x.setter
+    def x(self, new_x):
+        if not isinstance(new_x, (int, float)):
+            raise TypeError("Coordinate must be an integer or float")
+        else:
+            if new_x > 10 or new_x < -10:
+                raise ValueError("Acceptable range is between 10 and -10")
+        self._x = new_x
+    @property
+    def y(self):
+        return self._y
+    @y.setter
+    def y(self, new_y):
+        if not isinstance(new_y, (int, float)):
+            raise TypeError("Coordinate must be an integer or float")
+        else:
+            if new_y > 10 or new_y < -10:
+                raise ValueError("Acceptable range is between 10 and -10")
+        self._y = new_y
+    @property
+    def height(self):
+        return self._height
+    @height.setter
+    def height(self, new_height):
+        if not isinstance(new_height, (int, float)):
+            raise TypeError("Height must be an integer or float")
+        elif new_height <= 0:
+            raise ValueError("Height must be greater than 0")
+        self._height = new_height
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self, new_width):
+        if not isinstance(new_width, (int, float)):
+            raise TypeError("Width must be an integer or float")
+        elif new_width <= 0:
+            raise ValueError("Width must be greater than 0")
+        self._width = new_width
+    @property
+    def area(self):
+        return f"Area of rectangle = {self.height * self.width}"
+    @property
+    def perimeter(self):
+        return f"Perimeter of rectangle = {self.height * 2 + self.width * 2}"
+    def __eq__(self, other):
+        return self.area_ == other.area_
+    def __lt__(self, other):
+        return self.area_ < other.area_
+    def __gt__(self, other):
+        return self.area_ > other.area_
+    def __le__(self, other):
+        return self.area_ <= other.area_
+    def __ge__(self, other):
+        return self.area_ >= other.area_
+    def __repr__(self):
+        return f"Rectangle({self.x}, {self.y})"
+    def __str__(self):
+        return f"Rectangle object with coordinates ({self.x}, {self.y})"
+    def translate(self, new_x, new_y):
+        if not isinstance(new_x, (int, float)) or not isinstance(new_y, (int, float)):
+            raise TypeError(f"Coordinates must be int or float")
+        else:
+            if 10 < new_x < -10 or 10 < new_y < -10:
+                raise ValueError("Acceptable range for coordinates is between 10 and -10")
+        self.x = new_x
+        self.y = new_y
+    def check_square(self):
+        if self.height == self.width:
+            return True
+        else:
+            return False
